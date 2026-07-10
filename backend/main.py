6a92b37e1992f3c6126d978e2a332148629c018f
PiyushJ1 <piyxsh@gmail.com>
2026-07-11 03:32:17 +1000

    feat: add add server id to identify different server instances

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+import os
+
+SERVER_ID = os.getenv("SERVER_ID", "X")
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"server": 1}
+    return {"server": SERVER_ID}
+
 
 @app.get("/health")
 async def health():
